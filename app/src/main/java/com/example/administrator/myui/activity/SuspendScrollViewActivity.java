f5c14fe390f3226d949103f2a01801685028fd14
Administrator <1570595476@qq.com>
2015-06-12 17:40:26 +0800

    悬浮布局

--- app/src/main/java/com/example/administrator/myui/activity/SuspendScrollViewActivity.java
+++ app/src/main/java/com/example/administrator/myui/activity/SuspendScrollViewActivity.java
@@ -3,7 +3,7 @@ package com.example.administrator.myui.activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
+import android.widget.LinearLayout;
 
 
 import com.example.administrator.myui.R;
@@ -12,16 +12,23 @@ import com.example.administrator.myui.ui.SuspendScrollView;
 public class SuspendScrollViewActivity extends ActionBarActivity implements SuspendScrollView.OnScrollYListener {
 
     private SuspendScrollView suspendScrollView;
+    private LinearLayout linearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suspend_scroll_view);
-
         suspendScrollView = (SuspendScrollView) findViewById(R.id.activity_suspend_acroll_scrollview);
-
+        linearLayout = (LinearLayout)findViewById(R.id.suspen_scroll_include);
         suspendScrollView.setonScrollYListener(this);
 
+
+    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        int top = linearLayout.getTop();
+        Log.e("距离", top +"");
+        super.onWindowFocusChanged(hasFocus);
     }
     @Override
     public void scaleYListener(float a) {
