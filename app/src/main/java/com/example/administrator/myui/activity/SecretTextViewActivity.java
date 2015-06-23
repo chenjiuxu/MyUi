46b7691102cb0a714e666a552e0281e7fecca99c
Administrator <1570595476@qq.com>
2015-06-23 15:40:01 +0800

    修改

--- app/src/main/java/com/example/administrator/myui/activity/SecretTextViewActivity.java
+++ app/src/main/java/com/example/administrator/myui/activity/SecretTextViewActivity.java
@@ -20,7 +20,7 @@ public class SecretTextViewActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secret_text_view);
         secretTextView = (SecretTextView) findViewById(R.id.activity_secret_text_view_secrettextview);
-        secretTextView.setText("cmowmcodmcovniedvjierjvbievjibhuvirth89vijribhrtvjiortjb9rtjvb90rtjnrt90jb9rt");
+        secretTextView.setText("经过一年多的发展，基于Mandriva的社区发行版Mageia非常自豪地在官网宣布了Mageia 5的到来Mageia 是基于 GNU/Linux 的自由软件操作系统。这是一项社区项目，由经推选的志愿者组成的非营利性机构所支持。 以下为官方博客文章摘译 我们选择花费大量的时间来解决重大问题，以此来获得一个高品质的版本，而不是急于求成。Mageia 5或许是迄今为止最好的版本，主要是安装更加地便捷顺利、增加了诸多新功能和修复了一系列的bug");
         secretTextView.setmDuration(3000);
         secretTextView.setIsVisible(true);
 
--- app/src/main/java/com/example/administrator/myui/fragment/LayoutFragment.java
+++ app/src/main/java/com/example/administrator/myui/fragment/LayoutFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.administrator.myui.R;
 import com.example.administrator.myui.activity.DragTopLayoutActivity;
 import com.example.administrator.myui.activity.FragmentCutPagerSlidingTabStripActivity;
